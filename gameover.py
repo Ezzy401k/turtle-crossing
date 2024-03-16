d26977876bf4fdb2230722b49d989dd303ec9454
Esrael Mekdem <esraelmekdem@gmail.com>
2024-03-16 12:35:27 +0300

    Update gameover.py

--- gameover.py
+++ gameover.py
@@ -1,7 +1,6 @@
 import turtle
 
-
 class GameOver(turtle.Turtle):
     def __init__(self):
-        super().__init__()
-        self.write("Game Over!", False, "center", ("Arial", 24, "normal"))
+        super().__init__()  # Initialize the turtle object
+        self.write("Game Over!", False, "center", ("Arial", 24, "normal"))  # Write "Game Over!" on the screen at the center, using Arial font, size 24, and normal style
