23005a164ffcb9c276833759e9b8d2baf7af7afc
Esrael Mekdem <esraelmekdem@gmail.com>
2024-03-16 12:37:19 +0300

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -2,22 +2,25 @@ import turtle
 
 FONT = ("Courier", 18, "normal")
 
-
 class ScoreBoard(turtle.Turtle):
     def __init__(self):
-        super().__init__()
-        self.penup()
-        self.hideturtle()
-        self.score_point = 1  # Initialize score_point attribute
+        super().__init__()  # Initialize the turtle object
+        self.penup()  # Lift the pen to avoid drawing lines
+        self.hideturtle()  # Hide the turtle cursor
+        self.score_point = 1  # Initialize the score_point attribute
+
+        # Call the update_score method to display the initial score
         self.update_score()
 
     def update_score(self):
+        # Update the score display
         self.clear()  # Clear previous score before updating
-        self.goto(-280, 250)
-        self.write(f"Level: ", False, font=FONT)
-        self.goto(-190, 250)
-        self.write(self.score_point, False, font=FONT)
+        self.goto(-280, 250)  # Set the position for displaying the "Level" text
+        self.write("Level: ", False, font=FONT)  # Display the "Level" text
+        self.goto(-190, 250)  # Set the position for displaying the score
+        self.write(self.score_point, False, font=FONT)  # Display the current score
 
     def point(self):
-        self.score_point += 1
-        self.update_score()
+        # Increase the score by 1 and update the score display
+        self.score_point += 1  # Increment the score
+        self.update_score()  # Update the score display
